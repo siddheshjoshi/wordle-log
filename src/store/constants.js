ccd188c6d519a518401b7aa35006fa23b287fc77
Siddhesh Joshi <siddheshmail@gmail.com>
2022-03-08 12:09:07 -0500

    wordle score#262

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,214 +1,295 @@
 const CONSTANTS = {
-  scores: [
-    {
-      wordleDay: "230",
-      playerScores: {
-        GM: 5,
-        AM: 4,
-        AJ: 5,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "231",
-      playerScores: {
-        GM: 3,
-        AM: 3,
-        AJ: 6,
-        SJ: 5,
-      },
-    },
-    {
-      wordleDay: "232",
-      playerScores: {
-        GM: 4,
-        AM: 4,
-        AJ: 4,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "233",
-      playerScores: {
-        GM: 6,
-        AM: 4,
-        AJ: 3,
-        SJ: 6,
-      },
-    },
-    {
-      wordleDay: "234",
-      playerScores: {
-        GM: 5,
-        AM: 3,
-        AJ: 6,
-        SJ: 5,
-      },
-    },
-    {
-      wordleDay: "235",
-      playerScores: {
-        GM: 4,
-        AM: 3,
-        AJ: 4,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "236",
-      playerScores: {
-        GM: 6,
-        AM: 6,
-        AJ: 3,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "237",
-      playerScores: {
-        GM: 6,
-        AM: 5,
-        AJ: 5,
-        SJ: 6,
-      },
-    },
-    {
-      wordleDay: "238",
-      playerScores: {
-        GM: 4,
-        AM: 4,
-        AJ: 3,
-        SJ: 3,
-      },
-    },
-    {
-      wordleDay: "239",
-      playerScores: {
-        GM: 3,
-        AM: 4,
-        AJ: 4,
-        SJ: 3,
-      },
-    },
-    {
-      wordleDay: "240",
-      playerScores: {
-        GM: 4,
-        AM: 4,
-        AJ: 5,
-        SJ: 5,
-      },
-    },
-    {
-      wordleDay: "241",
-      playerScores: {
-        GM: 3,
-        AM: 5,
-        AJ: 3,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "243",
-      playerScores: {
-        GM: 3,
-        AM: 3,
-        AJ: 4,
-        SJ: 3,
-      },
-    },
-    {
-      wordleDay: "244",
-      playerScores: {
-        GM: 3,
-        AM: 6,
-        AJ: 3,
-        SJ: 3,
-      },
-    },
-    {
-      wordleDay: "245",
-      playerScores: {
-        GM: 6,
-        AM: 6,
-        AJ: 5,
-        SJ: 6,
-      },
-    },
-    {
-      wordleDay: "246",
-      playerScores: {
-        GM: 5,
-        AM: 3,
-        AJ: 6,
-        SJ: 4,
-      },
-    },
-    {
-      wordleDay: "247",
-      playerScores: {
-        GM: 6,
-        AM: 2,
-        AJ: 4,
-        SJ: 4,
-      },
-    } ,
-    {
-      wordleDay: "248",
-      playerScores: {
-        GM: 4,
-        AM: 3,
-        AJ: 4,
-        SJ: 4,
-      },
-    }  ,
-    {
-      wordleDay: "249",
-      playerScores: {
-        GM: 5,
-        AM: 4,
-        AJ: 6,
-        SJ: 6,
-      },
-    }  ,
-    {
-      wordleDay: "250",
-      playerScores: {
-        GM: 5,
-        AM: 4,
-        AJ: 3,
-        SJ: 3,
-      },
-    }  ,
-    {
-      wordleDay: "251",
-      playerScores: {
-        GM: 5,
-        AM: 5,
-        AJ: 6,
-        SJ: 4,
-      },
-    }  ,
-    {
-      wordleDay: "252",
-      playerScores: {
-        GM: 6,
-        AM: 6,
-        AJ: 6,
-        SJ: 3,
-      },
-    },
-    {
-      wordleDay: "253",
-      playerScores: {
-        GM: 3,
-        AM: 2,
-        AJ: 4,
-        SJ: 5,
-      },
-    }
+    scores: [
+        {
+            wordleDay: "230",
+            playerScores: {
+                GM: 5,
+                AM: 4,
+                AJ: 5,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "231",
+            playerScores: {
+                GM: 3,
+                AM: 3,
+                AJ: 6,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "232",
+            playerScores: {
+                GM: 4,
+                AM: 4,
+                AJ: 4,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "233",
+            playerScores: {
+                GM: 6,
+                AM: 4,
+                AJ: 3,
+                SJ: 6,
+            },
+        },
+        {
+            wordleDay: "234",
+            playerScores: {
+                GM: 5,
+                AM: 3,
+                AJ: 6,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "235",
+            playerScores: {
+                GM: 4,
+                AM: 3,
+                AJ: 4,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "236",
+            playerScores: {
+                GM: 6,
+                AM: 6,
+                AJ: 3,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "237",
+            playerScores: {
+                GM: 6,
+                AM: 5,
+                AJ: 5,
+                SJ: 6,
+            },
+        },
+        {
+            wordleDay: "238",
+            playerScores: {
+                GM: 4,
+                AM: 4,
+                AJ: 3,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "239",
+            playerScores: {
+                GM: 3,
+                AM: 4,
+                AJ: 4,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "240",
+            playerScores: {
+                GM: 4,
+                AM: 4,
+                AJ: 5,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "241",
+            playerScores: {
+                GM: 3,
+                AM: 5,
+                AJ: 3,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "243",
+            playerScores: {
+                GM: 3,
+                AM: 3,
+                AJ: 4,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "244",
+            playerScores: {
+                GM: 3,
+                AM: 6,
+                AJ: 3,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "245",
+            playerScores: {
+                GM: 6,
+                AM: 6,
+                AJ: 5,
+                SJ: 6,
+            },
+        },
+        {
+            wordleDay: "246",
+            playerScores: {
+                GM: 5,
+                AM: 3,
+                AJ: 6,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "247",
+            playerScores: {
+                GM: 6,
+                AM: 2,
+                AJ: 4,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "248",
+            playerScores: {
+                GM: 4,
+                AM: 3,
+                AJ: 4,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "249",
+            playerScores: {
+                GM: 5,
+                AM: 4,
+                AJ: 6,
+                SJ: 6,
+            },
+        },
+        {
+            wordleDay: "250",
+            playerScores: {
+                GM: 5,
+                AM: 4,
+                AJ: 3,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "251",
+            playerScores: {
+                GM: 5,
+                AM: 5,
+                AJ: 6,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "252",
+            playerScores: {
+                GM: 6,
+                AM: 6,
+                AJ: 6,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "253",
+            playerScores: {
+                GM: 3,
+                AM: 2,
+                AJ: 4,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "254",
+            playerScores: {
+                GM: 5,
+                AM: 6,
+                AJ: 5,
+                SJ: 3,
+            },
+        },
+        {
+            wordleDay: "255",
+            playerScores: {
+                GM: 3,
+                AM: 5,
+                AJ: 3,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "256",
+            playerScores: {
+                GM: 2,
+                AM: 3,
+                AJ: 4,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "257",
+            playerScores: {
+                GM: 3,
+                AM: 3,
+                AJ: 6,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "258",
+            playerScores: {
+                GM: 4,
+                AM: 3,
+                AJ: 5,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "259",
+            playerScores: {
+                GM: 6,
+                AM: 3,
+                AJ: 3,
+                SJ: 2,
+            },
+        },
+        {
+            wordleDay: "260",
+            playerScores: {
+                GM: 6,
+                AM: 6,
+                AJ: 3,
+                SJ: 5,
+            },
+        },
+        {
+            wordleDay: "261",
+            playerScores: {
+                GM: 3,
+                AM: 3,
+                AJ: 5,
+                SJ: 4,
+            },
+        },
+        {
+            wordleDay: "262",
+            playerScores: {
+                GM: 5,
+                AM: 3,
+                AJ: 4,
+                SJ: 3,
+            },
+        }
 
-  ],
+    ],
 };
 
 export default CONSTANTS;
